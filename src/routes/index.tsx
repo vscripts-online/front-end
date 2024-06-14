d8096bae40596773a6f3feb621666425401cdba4
Erdem <22412769+virtueer@users.noreply.github.com>
2024-06-14 23:41:21 +0300

    index page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { useAuthAPIQuery } from "@/auth";
 import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
 import { Link, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -11,12 +12,41 @@ function Onboard() {
   const { data } = GetMe();
 
   return (
-    <Navbar>
+    <>
+      <Navbar>
+        {data && (
+          <Link to="/dashboard" disabled={!data}>
+            Dashboard
+          </Link>
+        )}
+      </Navbar>
       {data && (
-        <Link to="/dashboard" disabled={!data}>
-          Dashboard
-        </Link>
+        <div className="mx-auto w-fit mt-52 text-center text-xl">
+          Did you see{" "}
+          <a
+            href="https://shorter.vscripts.online"
+            className="underline underline-offset-2 font-semibold"
+          >
+            Shorter
+          </a>{" "}
+          service?
+          <br />
+          <a href="https://shorter.vscripts.online">
+            <Button className="mt-5 h-auto bg-red-600 text-white border">
+              Take a look
+            </Button>
+          </a>
+        </div>
       )}
-    </Navbar>
+      {!data && (
+        <div className="mx-auto w-fit mt-52 text-center text-xl">
+          Sign now and get{" "}
+          <span className="font-semibold underline underline-offset-2">
+            500 MB
+          </span>{" "}
+          free storage
+        </div>
+      )}
+    </>
   );
 }
